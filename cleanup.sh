3cdeab614cd20c5f1d5eca7c68b3d22405b57ab1
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-11-26 00:44:46 +0300

    testing obj-c in cmake

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SUPPORT="test-xcode/Demo/Support"
+
+rm -rf ${SUPPORT}/Python-3.10-macOS-support.*
+rm -rf ${SUPPORT}/python-stdlib
+rm -rf ${SUPPORT}/Python.xcframework
+rm -rf ${SUPPORT}/VERSIONS
+
