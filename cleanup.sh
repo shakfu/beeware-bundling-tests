bc71a0fe54fb9f485356ee9dd966a7e96c3747b6
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-11-26 04:49:54 +0300

    added objc cmake test

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 SUPPORT="test-xcode/Demo/Support"
+SUBPROJECTS="test-cmake test-cmake-objc"
 
 rm -rf ${SUPPORT}/Python-3.10-macOS-support.*
 rm -rf ${SUPPORT}/python-stdlib
 rm -rf ${SUPPORT}/Python.xcframework
 rm -rf ${SUPPORT}/VERSIONS
 
+for target in ${SUBPROJECTS}
+do
+	echo "cleaning: ${target}"
+	rm -rf ${target}/build
+done
---
+++ test-cmake-objc/README.md
@@ -0,0 +1,6 @@
+# About this test
+
+Attempt to build a build using cmake, beeware statically compiled python and c-api entry point.
+
+
+
